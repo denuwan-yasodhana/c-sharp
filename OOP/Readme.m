1074d55a2d92c2c7cfbd4082cca0e93812079a25
Denuwan Yasodhana <Denuwan_Yasodhana77@outlook.com>
2023-06-05 07:52:04 +0530

    Create Readme.m

---
+++ OOP/Readme.m
@@ -0,0 +1,11 @@
+Here are some real-world examples where abstract OOP concepts are commonly used:
+
+01). Banking Systems: In a banking system, you can have an abstract class called Account with concrete subclasses like SavingsAccount and CheckingAccount. The abstract class can define common properties and methods such as accountNumber and withdraw(), while the subclasses can provide their own specific implementations.
+
+02). Graphic User Interfaces (GUI): GUI frameworks often use abstract classes and interfaces to define common behaviors and provide a consistent user experience. For example, you might have an abstract class called Component with subclasses like Button and TextField, each providing their own implementations for rendering and handling user interactions.
+
+03). Vehicle Manufacturing: When designing a system to manufacture vehicles, you could have an abstract class called Vehicle with subclasses like Car and Truck. The abstract class can define shared properties and methods like startEngine() and stopEngine(), while the subclasses can provide specialized implementations for specific vehicle types.
+
+04). Online Marketplace: In an online marketplace application, you might have an abstract class called Product with subclasses like Book, Electronics, and Clothing. The abstract class can define common attributes like price and methods like getDiscount(), and the subclasses can provide additional functionality specific to each product type.
+
+05). Game Development: Game development often relies heavily on abstract OOP concepts. For example, you might have an abstract class called GameObject with subclasses like Player, Enemy, and Obstacle. The abstract class can define shared behaviors like update() and render(), while the subclasses can provide their own implementations for unique characteristics and interactions.
